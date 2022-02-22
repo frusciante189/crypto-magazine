a9803666dac591dcc52c8a51e32db1dafc59f1cf
Frus <mcagdasbayirli@gmail.com>
2022-02-22 19:57:41 +0300

    fixes

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -15,11 +15,6 @@ const Index = ({ categories }) => {
 
   return (
     <>
-      <Head>
-        <title className="capitalize">
-          Fruschain - {capitalize(router.query.slug)}
-        </title>
-      </Head>
       <Navbar categories={categories} />
       <div className="dark:bg-darkBg bg-white transition-all transform duration-500 ease-in-out lg:py-6 sm:py-4 py-2">
         <div className="max-w-screen-2xl mx-auto lg:px-8 sm:px-6 px-4 ">
